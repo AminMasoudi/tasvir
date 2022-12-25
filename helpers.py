0d01bfa6429a6dde4dc9d8c334a207c0e21b6605
AminMasoudi <aminmasoudi2003@gmail.com>
2022-12-25 14:56:47 +0330

    helpers 	apology+loginrequired 	Almost DONE

--- helpers.py
+++ helpers.py
@@ -1,1 +1,22 @@
+from flask import render_template, redirect, session
+from functools import wraps
+
+
 #TODO : apology
+
+
+def apology(message, code=400):
+    #def escape(s):
+        # for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+        #                  ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+        #     s = s.replace(old, new)
+        # return s
+    return render_template("apology.html",top = code, message=message), code
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
