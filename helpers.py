01bc97f3760cf8960013a9854124714e1d5b5aae
AminMasoudi <aminmasoudi2003@gmail.com>
2022-12-28 22:18:44 +0330

    addresses changed save, load_waited_list, discribe_splite added TODO : upgrade()

--- helpers.py
+++ helpers.py
@@ -1,8 +1,12 @@
 from flask import render_template, redirect, session
 from functools import wraps
-import os
+from werkzeug.utils import secure_filename
 
-#TODO : apology
+import os
+import sqlite3 as sql
+
+con = sql.connect("Final.db",check_same_thread=False)
+db = con.cursor()
 
 
 def apology(message, code=400):
@@ -23,5 +27,29 @@ def login_required(f):
     return decorated_function
 
 def addresses():
-    files = os.listdir("static/load")
-    return ["static/load/"+ i for i in files]
+    files = os.listdir("static/img")
+    return ["static/img/"+ i for i in files]
+
+def save(file, name="anonimus", discire="no discription"):
+    try:
+        no = db.execute("SELECT COUNT(*) FROM UPLOAD;").fetchall()[0][0]
+        addr = "static/img/" + secure_filename(file.filename)
+        file.save(addr)
+        db.execute("INSERT INTO upload (no, name, discribe, addr) VALUES(? ,?, ?, ?);",(no ,name,discire,addr))
+        return 0
+    except:
+        return 1
+
+def upgrade():
+    return
+
+def discribe_spilit(my_tuple):
+    my_list = list(my_tuple)
+    my_list[2] = my_list[2].split("\n")
+    return my_list
+    
+
+def load_waited_img():
+    img_list = db.execute("SELECT * FROM upload ORDER BY no").fetchall()
+    img_list = list(map(lambda x : discribe_spilit(x), img_list))
+    return img_list
