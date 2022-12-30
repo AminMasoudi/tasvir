a805b272054fdbad8b7c5674a90d6da837184eee
AminMasoudi <aminmasoudi2003@gmail.com>
2022-12-30 11:14:48 +0330

    Addresses >> debuged + upgraded con.commit() added after each db INSERT or DELETE execution def upgrade >> completed def delete >> added

--- helpers.py
+++ helpers.py
@@ -9,12 +9,8 @@ con = sql.connect("Final.db",check_same_thread=False)
 db = con.cursor()
 
 
-def apology(message, code=400):
-    #def escape(s):
-        # for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-        #                  ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-        #     s = s.replace(old, new)
-        # return s
+def apology(message:str ="TODO", code=400):
+    
     return render_template("apology.html",top = code, message=message), code
 
 #TODO : Login_required
@@ -27,8 +23,10 @@ def login_required(f):
     return decorated_function
 
 def addresses():
-    files = os.listdir("static/img")
-    return ["static/img/"+ i for i in files]
+    address = db.execute("SELECT * FROM load ORDER BY NO DESC;").fetchall()
+    address = list(map(lambda x:discribe_spilit(x), address))
+    return address
+
 
 def save(file, name="anonimus", discire="no discription"):
     try:
@@ -36,12 +34,25 @@ def save(file, name="anonimus", discire="no discription"):
         addr = "static/img/" + secure_filename(file.filename)
         file.save(addr)
         db.execute("INSERT INTO upload (no, name, discribe, addr) VALUES(? ,?, ?, ?);",(no ,name,discire,addr))
+        con.commit()
         return 0
     except:
         return 1
 
-def upgrade():
-    return
+def upgrade(id:str):
+    
+    try:
+        row = db.execute("SELECT * FROM upload WHERE no=?",id).fetchall()[0]
+        row = list(row)
+        no = db.execute("SELECT COUNT(*) FROM load ;").fetchall()[0][0]
+        db.execute("INSERT INTO load(no, name, discribe, addr) VALUES(?, ?, ?, ?);",(no, row[1], row[2], row[3]))
+        db.execute("DELETE FROM upload WHERE no=?;",id)
+        con.commit()
+        
+        return redirect("/admin")
+    except:    
+        return apology("db problem!!")
+    
 
 def discribe_spilit(my_tuple):
     my_list = list(my_tuple)
@@ -53,3 +64,14 @@ def load_waited_img():
     img_list = db.execute("SELECT * FROM upload ORDER BY no").fetchall()
     img_list = list(map(lambda x : discribe_spilit(x), img_list))
     return img_list
+
+def delete(id:str):
+    try:
+        #TODO : delete img
+        file_path = db.execute("SELECT addr FROM upload WHERE no=?",id).fetchall()[0][0]
+        os.remove(file_path)
+        db.execute("DELETE FROM upload WHERE no=?;",id)
+        con.commit()
+        return redirect("/admin")
+    except:
+        return apology("couldn't delete img from db our system")
